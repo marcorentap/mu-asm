8802408969c705f79b8a9f5933fe1a030a05c0a0
Marc Owen <marcowen20@gmail.com>
2023-07-27 03:13:15 +0900

    Renamed assembly file extension

--- test.asm
+++ test.asm
@@ -5,7 +5,12 @@ SET R1 0x03
 SET R2 0x04
 SET R0 RIP
 CALL @Function1 ; Call function1(3, 4)
-J @exit
+
+SET R1 0x13
+SET R2 0x37
+SET R0 RIP
+CALL @Function1 ; Call function1(13, 37)
+J @loop
 
 @Function1
 ; use R0 as return, R1 as arg1, R2 as arg2 and they are caller-saved registers
@@ -21,11 +26,11 @@ ST4 RSP 0x0000c0de
 
 ; Calculate x*t + y
 LD4 R0 RSP
-MULT R0 R1
+MUL R0 R1
 ADD R0 R2
 ADD RSP 0x04
 RET
 ; Function 1 end
 
-@exit
-HALT
+@loop
+J @loop
